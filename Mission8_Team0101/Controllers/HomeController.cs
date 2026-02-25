7650a017d3634e4a190eaa56a97cd1b21693b269
gmnelson <gmnelson@byu.edu>
2026-02-24 18:21:16 -0700

    Implemented abstracting context file, wrapping it up as per videos.

--- Mission8_Team0101/Controllers/HomeController.cs
+++ Mission8_Team0101/Controllers/HomeController.cs
@@ -6,8 +6,14 @@ namespace Mission8_Team0101.Controllers;
 
 public class HomeController : Controller
 {
- 
+    private IHabit _repo; // Abstracted the context file, best practice. Wrapped in a box, can't see it in the class. Industry Standard.
+
+    public HomeController(IHabit temp)
+    {
+        _repo = temp;
+    }
     
+    // TODO: Waiting on Models / Database / Views to be configured.
     public IActionResult Index()
     {
         return View();
